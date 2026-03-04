22c91c53dbc0fec24a4f0bd3633aa40a159e5288
Claude Stolze <claude.stolze@uniud.it>
2026-03-04 15:54:05 +0100

    simplify synpa_js.ml

--- bin/synpa_js.ml
+++ bin/synpa_js.ml
@@ -4,7 +4,7 @@ open Synpa
 let draw s =
   Js.Unsafe.fun_call
     (Js.Unsafe.js_expr "draw")
-    [| Js.Unsafe.inject s |]
+    [| Js.Unsafe.inject (Js.string s) |]
 
 let alert s =
   Js.Unsafe.fun_call
@@ -17,23 +17,10 @@ let install g n =
   | Some f ->
     begin
       g##.onclick := Dom_html.handler (fun _ ->
-          f (); draw (Js.string (Graph.graphviz ())));
+          f (); draw (Graph.graphviz ()));
       g##.classList##add (Js.string "clickme")
     end
   | None -> ()
-
-let _example () =
-  let _p = Parallel.parallel (Prefix.prefix (Tts.Pos("a")) [] (Zero.zero [])) (Prefix.prefix (Tts.Neg("a")) [] (Zero.zero [])) in
-  let cell = Sum.sum (Sum.sum (Prefix.prefix (Tts.Neg "r") [Tts.Neg "w"; Tts.Neg "r"] (Name.name "cell" ["sigma"])) (Prefix.prefix (Tts.Neg "w") [Tts.Neg "w"] (Prefix.prefix (Tts.Clock "sigma") [] (Name.name "cell" ["sigma"])))) (Prefix.prefix (Tts.Clock "sigma") [Tts.Neg "w"; Tts.Neg "r"] (Name.name "cell" ["sigma"])) in
-  let cell_name = Name.name "cell" ["sigma"] in
-  let p = Prefix.prefix (Tts.Pos "r") [] (Zero.zero ["sigma"]) in
-  let q = Prefix.prefix (Tts.Pos "w") [] (Zero.zero ["sigma"]) in
-  let total = Parallel.parallel (Parallel.parallel cell_name p) q in
-  begin
-    Name.install "cell" cell;
-    Graph.new_graph total;
-    Graph.graphviz ()
-  end
 
 let parse s =
   Name.clear ();
